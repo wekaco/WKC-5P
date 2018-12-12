f4afb729a82cbff6fe8525d558f4c108703ae32a
Dmtrs Flaco Mengidis <tydeas.dr@gmail.com>
2018-12-12 11:53:17 +0200

    ci(tslint): Added tslint on pre:test

--- labs/osc/index.ts
+++ labs/osc/index.ts
@@ -14,7 +14,16 @@ import { msg } from "supercolliderjs";
 const REMOTE_ADDRESS = "127.0.0.1";
 const REMOTE_PORT = 57110;
 
+port.on("listening", (): void  => {
+  debug(`Socket listening`);
+});
+port.on("error", (err: Error): void => {
+  debug(`Socket error: ${err}`);
+});
 port.on("message", handler);
+port.on("close", (): void => {
+  debug(`Socket closed`);
+});
 
 /**
  * let numFrames = 32;
@@ -23,9 +32,9 @@ port.on("message", handler);
 
 for (let bufferID: number = 0; bufferID < 4; bufferID++) {
   const call: Array<string> = msg.bufferSet(bufferID, [
-    [ 0, 0.2],
-    [ 1, 0.3],
-    [ 7, 1],
+    [0, 0.2],
+    [1, 0.3],
+    [7, 1],
   ]);
   const b: Buffer = buf(call, MessageType.message);
 
