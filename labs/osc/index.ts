c369c254ecac8db7e82b17e32da19a3ee78656a8
Dmtrs Flaco Mengidis <tydeas.dr@gmail.com>
2018-12-23 00:39:02 +0200

    docs(TODO): Added TODO list in index.ts

--- labs/osc/index.ts
+++ labs/osc/index.ts
@@ -5,6 +5,18 @@ import { Client, ServerOptions } from "./src/client";
 
 const client = new Client({ remote_address: "127.0.0.1", remote_port: 57110 });
 
+client.on("ready", () => {
+  debug(`client ready`);
+});
+
+/** TODO:
+ * - Process exit handling (notify state off?)
+ * - Buffer allocator
+ * - Buffer stream
+ * - Buffer clusterization
+ */
+
+
 // client.send(b).then(debug);
 /**
  * let numFrames = 32;
